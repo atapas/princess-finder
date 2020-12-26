e38219b846284167efbf673c27314a8585e578f4
Tapas Adhikary <tapas.adhikary@gmail.com>
2020-12-26 13:23:48 +0530

    Fixed a small warning

--- src/Chart.js
+++ src/Chart.js
@@ -4,7 +4,7 @@ import GaugeChart from "react-gauge-chart";
 const Chart = (props) => {
   const data = props.data;
   const label = data.label;
-  const confidence = parseFloat(data.confidence).toFixed(2);
+  const confidence = parseFloat(data.confidence.toFixed(2));
   console.log(label, confidence);
   return (
     <div>
